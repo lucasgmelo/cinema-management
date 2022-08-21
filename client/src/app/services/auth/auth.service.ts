b7f574a92ba58c705fd594a3b64203599a9e7929
Lucas Melo <contatolucasgmelo@gmail.com>
2022-08-21 18:50:41 -0300

    feat(navbar): add if logical and autologin

---
+++ client/src/app/services/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+export interface User {
+  name: string | null;
+  access: 'guest' | 'manager' | 'customer';
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {
+    if (localStorage.getItem('user')) {
+      const currentUser: User = JSON.parse(
+        localStorage.getItem('user') || '{}'
+      );
+
+      this.user.name = currentUser.name;
+      this.user.access = currentUser.access;
+    }
+  }
+
+  user: User = {
+    name: null,
+    access: 'guest',
+  };
+
+  signIn(name: string, access: 'manager' | 'customer') {
+    console.log('123');
+    localStorage.setItem('user', JSON.stringify({ name, access }));
+    this.user.name = name;
+    this.user.access = access;
+  }
+
+  signOut() {
+    this.user.name = null;
+    this.user.access = 'guest';
+
+    localStorage.removeItem('user');
+  }
+}
