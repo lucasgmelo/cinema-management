d535ec3a2a167e3a2b30d05db985881adbedcb8f
artursanntos <arturcs2001@gmail.com>
2022-08-30 17:25:48 -0300

    feat(main): create badge component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { BadgeComponent } from './common/badge/badge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    BadgeComponent
   ],
   imports: [
     BrowserModule,
