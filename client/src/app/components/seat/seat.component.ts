a2c591ba4cb78e4f3c96d6fbaa06390a0afd7fbe
Lucas Melo <contatolucasgmelo@gmail.com>
2022-09-01 17:03:05 -0300

    feat(main): fix toggle

--- client/src/app/components/seat/seat.component.ts
+++ client/src/app/components/seat/seat.component.ts
@@ -3,28 +3,22 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-seat',
   templateUrl: './seat.component.html',
-  styleUrls: ['./seat.component.scss']
+  styleUrls: ['./seat.component.scss'],
 })
-
 export class SeatComponent implements OnInit {
-
   @Input() isDisabled!: boolean;
 
   state: 'default' | 'selected' = 'default';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  toggle(){
-    
-    if(this.state == 'default'){
+  toggle() {
+    if (this.state == 'default') {
       this.state = 'selected';
-
-    }else{
+    } else {
       this.state = 'default';
     }
   }
-
 }
