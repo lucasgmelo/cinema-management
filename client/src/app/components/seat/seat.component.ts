272e3a0a7985cb46dc801f8b2c141ffd3aee9a61
Lucas Oliveira Cavalcanti <loc@WINDOWS.CIN.UFPE.BR>
2022-09-01 16:23:15 -0300

    feat(main) feature seat

--- client/src/app/components/seat/seat.component.ts
+++ client/src/app/components/seat/seat.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-seat',
   templateUrl: './seat.component.html',
   styleUrls: ['./seat.component.scss']
 })
+
 export class SeatComponent implements OnInit {
+
+  @Input() isDisabled!: boolean;
+
+  state: 'default' | 'selected' = 'default';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle(){
+    
+    if(this.state == 'default'){
+      this.state = 'selected';
+
+    }else{
+      this.state = 'default';
+    }
   }
 
 }
